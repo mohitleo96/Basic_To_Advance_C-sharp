e019bd3524e3c7c5703ba978d043aac21147d20a
Mohit rao <mmohitrrao96@gmail.com>
2025-10-23 22:54:46 +0530

    Q1-Q5 Added

---
+++ 2_IntermediateQuestions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace _2_IntermediateQuestions
+{
+class Program{
+    public static void Main(string[] main)
+        {
+            // Console.WriteLine("Enter First Number");
+            // int x = Convert.ToInt32(Console.ReadLine());
+            //  Console.WriteLine("Enter Second Number");
+            // int y = Convert.ToInt32(Console.ReadLine());
+            //  Console.WriteLine("Enter Third Number");
+            // int z = Convert.ToInt32(Console.ReadLine());
+            // Q1_largestofthreenumber large = new Q1_largestofthreenumber();
+            // large.Set_largestNumber(x, y, z);
+            // Console.WriteLine("The largest Number is {0}",large.Get_largestNumber());
+            //Q2_PrimeNumber.PrimeNumber();
+            //Q3_Factorial_Number.factorial();
+            //Q4_Reverse_Number.reverse();
+            Q5_Reverse_String.reverse();
+
+        }
+}
+}
